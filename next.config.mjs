09621bc3a6f32bf4f76bc0b38bab19f6f51725aa
poses98 <posesand@gmail.com>
2024-05-08 19:12:46 +0200

    chore: Add remote patterns for images in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/gv7y171m/production/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
